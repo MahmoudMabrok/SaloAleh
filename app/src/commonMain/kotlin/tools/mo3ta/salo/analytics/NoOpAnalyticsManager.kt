2801f0b4851ce82291b3a95f970f9dff5057467b
mahmoudmabrok <mahmoudmabrok3579@gmail.com>
2026-05-06 11:12:39 +0300

    fix: add App Attest entitlement, fix .gitignore for Podfile.lock, fix NoOpAnalyticsManager

--- app/src/commonMain/kotlin/tools/mo3ta/salo/analytics/NoOpAnalyticsManager.kt
+++ app/src/commonMain/kotlin/tools/mo3ta/salo/analytics/NoOpAnalyticsManager.kt
@@ -2,4 +2,5 @@ package tools.mo3ta.salo.analytics
 
 class NoOpAnalyticsManager : AnalyticsManager {
     override fun logAction(name: String, params: Map<String, String>) = Unit
+    override fun logView(name: String, params: Map<String, String>) = Unit
 }
