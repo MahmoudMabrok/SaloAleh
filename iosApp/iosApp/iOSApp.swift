24559e67af2b8e3b671fadfcd60738c6e69313a7
mahmoudmabrok <mahmoudmabrok3579@gmail.com>
2026-05-06 11:03:53 +0300

    feat: init Firebase App Check in iOS app

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,9 +1,15 @@
 import SwiftUI
 import FirebaseCore
+import FirebaseAppCheck
 
 @main
 struct iOSApp: App {
     init() {
+        #if DEBUG
+        AppCheck.setAppCheckProviderFactory(AppCheckDebugProviderFactory())
+        #else
+        AppCheck.setAppCheckProviderFactory(AppAttestProviderFactory())
+        #endif
         FirebaseApp.configure()
     }
 
