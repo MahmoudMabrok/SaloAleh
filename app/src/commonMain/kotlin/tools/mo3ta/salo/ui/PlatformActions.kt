51cc5ed445d0e546f6d661b9f6d06c42dec4f155
mahmoudmabrok <mahmoudmabrok3579@gmail.com>
2026-05-08 01:24:08 +0300

    feat: add shareImage expect declaration

--- app/src/commonMain/kotlin/tools/mo3ta/salo/ui/PlatformActions.kt
+++ app/src/commonMain/kotlin/tools/mo3ta/salo/ui/PlatformActions.kt
@@ -1,8 +1,11 @@
 package tools.mo3ta.salo.ui
 
+import tools.mo3ta.salo.ui.components.ShareCardData
+
 expect fun showPlatformToast(message: String)
 expect fun copyToClipboard(text: String)
 expect fun shareText(text: String)
 expect fun areNotificationsEnabled(): Boolean
 expect fun openNotificationSettings()
 expect fun getAppVersion(): String
+expect fun shareImage(data: ShareCardData)
