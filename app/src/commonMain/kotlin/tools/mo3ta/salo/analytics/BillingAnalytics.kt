804462376db8768bc502dbbc5e1d4773e4af3fe2
mahmoudmabrok <mahmoudmabrok3579@gmail.com>
2026-05-19 07:00:00 +0300

    feat: add billing analytics event constants

---
+++ app/src/commonMain/kotlin/tools/mo3ta/salo/analytics/BillingAnalytics.kt
@@ -0,0 +1,14 @@
+package tools.mo3ta.salo.analytics
+
+object BillingAnalytics {
+    const val PAYWALL_VIEWED = "paywall_viewed"
+    const val PURCHASE_STARTED = "purchase_started"
+    const val PURCHASE_COMPLETED = "purchase_completed"
+    const val PURCHASE_FAILED = "purchase_failed"
+    const val PURCHASE_RESTORED = "purchase_restored"
+    const val SCORE_MASK_TOGGLED = "score_mask_toggled"
+
+    const val PARAM_PRODUCT_ID = "product_id"
+    const val PARAM_ERROR = "error"
+    const val PARAM_ENABLED = "enabled"
+}
